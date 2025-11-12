76fb109e4ea8324f7817c613ba11999fc55528bb
MedCharaf <bouderraoui45@gmail.com>
2025-11-12 20:39:12 +0100

    credit data kaggle

--- day00/ex01.py
+++
@@ -1,7 +0,0 @@
-import time
-from time import gmtime, strftime
-from datetime import datetime
-
-seconds = time.time()
-print("second since january 1 , 1970: ", seconds ," or ",f"{seconds:.3e}  in a scientific notation", )
-print(time.ctime(seconds))
